86c9b695ee6e6e3c43b153561c80fdc0e5947fc7
Ahmad Yogi <ahmadyogi543@gmail.com>
2023-01-31 16:44:36 +0800

    chore: remove unused comment

--- Services/RemoveTodo.php
+++ Services/RemoveTodo.php
@@ -11,7 +11,6 @@ function remove_todo($id): bool
   for ($i = $id; $i < count($todos); $i++) {
     $todos[$i] = $todos[$i + 1];
   }
-  // unset($todos[count($todos)]);
   unset($todos[count($todos)]);
 
   return true;
